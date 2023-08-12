85c9821a45a6d6532adc3b689d382d1bd83370d0
Dennis <dtx319@gmail.com>
2023-08-11 18:34:42 -0700

    openai backend

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log(123);
+const express = require('express');
+const dotenv = require('dotenv').config;
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+// Enable body parser
+app.use(express.json());
+app.use(express.urlencoded({extended: false }))
+
+app.use('/openai', require('./routes/openaiRoutes'));
+
+app.listen(port, () => console.log(`Server started started on port ${port}`));

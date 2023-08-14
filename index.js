16b431cc61b034471ad27fc62174afcd547c1a5b
Dennis <dtx319@gmail.com>
2023-08-14 15:38:10 -0700

    fixed issue in index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const path = require('path');
 const express = require('express');
-const dotenv = require('dotenv').config;
+const dotenv = require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 const app = express();
 
 // Enable body parser
 app.use(express.json());
-app.use(express.urlencoded({extended: false }));
+app.use(express.urlencoded({ extended: false }));
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));

73a448cf026e09f0e72b0d8918227e5a1d2bcde3
Dennis <dtx319@gmail.com>
2023-08-14 14:30:38 -0700

    added spinner.css, style.css, and index.html

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv').config;
 const port = process.env.PORT || 5000;
@@ -6,7 +7,10 @@ const app = express();
 
 // Enable body parser
 app.use(express.json());
-app.use(express.urlencoded({extended: false }))
+app.use(express.urlencoded({extended: false }));
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/openai', require('./routes/openaiRoutes'));
 
